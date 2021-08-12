79a3847268c768f45bf0bdc64571a2940624fc80
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-12 16:34:11 -0500

    Added the ability for the item to display

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -1,6 +1,7 @@
 import ShoppingItem from '../ShoppingItem/ShoppingItem'
 function ShoppingList ({shoppingList}) {
     return (
+        
       <ul>
           {/* Pass feature components to this function */}
         {shoppingList.map(item => <ShoppingItem item ={item}/>)}
