c0ef99b83c3002744cbf215816ad4a4af57f7f66
Dan St. Aubin <staubindj@gmail.com>
2021-08-12 13:23:56 -0500

    added ShoppingItem component

---
+++ src/components/ShoppingItem/ShoppingItem.jsx
@@ -0,0 +1,14 @@
+function ShoppingItem(item) {
+
+    return (
+        <li>
+            item.item <br></br>item.quantity item.unit
+            <button>Buy</button>
+            <button>Remove</button>
+        </li>
+    );
+};
+
+
+
+export default ShoppingItem;
